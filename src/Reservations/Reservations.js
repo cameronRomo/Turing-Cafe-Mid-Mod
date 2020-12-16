10f4f704c7659e958b596429fc6d3380d7116413
Cameron Romo <cromo1225@gmail.com>
2020-12-16 10:32:06 -0700

    Add Reservations functional component to render reservation cards

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Reservations.css';
+import { Card } from '../Card/Card'
 
 const Reservations = ({ reservations }) => {
+
+  const reservationCards = reservations.map(reservation => {
+    return (
+      <Card 
+        name={ reservation.name }
+        date={ reservation.date }
+        time={ reservation.time }
+        number={ reservation.number }
+        id={ reservation.id}
+        key={ reservation.id }
+      />
+    )
+  })
+
   return (
-    <h2>Reservations go Here</h2>
+    <section className='reservations-container'>
+      { reservationCards }
+    </section>
   )
 }
 export { Reservations };
